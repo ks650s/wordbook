71b693e7c1e4ec1f407fc17035d3b22faca7b697
ks650s <seosadou40@gmail.com>
2025-03-19 05:26:58 +0000

    0319

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,20 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import { Application } from "@hotwired/stimulus" // Should exist already
+import RailsNestedForm from '@stimulus-components/rails-nested-form'
+
+const application = Application.start() // Should exist already
+application.register('nested-form', RailsNestedForm)
 import './add_jquery'
 import "@nathanvda/cocoon"
 import "@hotwired/turbo-rails"
 import "controllers"
-import "custom/image_upload"
+import "../custom/image_upload"
 require("jquery")
 require("@nathanvda/cocoon")
 import jquery from "jquery"
 window.jQuery = jquery
 window.$ = jquery
+//= require jquery_nested_form
 
 (function(){
   alert("jQuery 動いた！")
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,8 +2,9 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@nathanvda/cocoon", to: "https://ga.jspm.io/npm:@nathanvda/cocoon@1.2.14/cocoon.js"
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js"
+pin "@stimulus-components/rails-nested-form", to: "https://ga.jspm.io/npm:@stimulus-components/rails-nested-form@5.0.0/dist/stimulus-rails-nested-form.mjs"
